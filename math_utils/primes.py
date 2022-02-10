7ab4cff47f8f585117ffc60ce9b3f2df167691da
James Song <98577974+js3820@users.noreply.github.com>
2022-02-10 17:20:21 +0000

    Exercise 2.3 passed

---
+++ math_utils/primes.py
@@ -0,0 +1,11 @@
+from math import sqrt
+
+def isprime(n):
+    if n <= 1:
+        return False
+    elif n == 2:
+        return True
+    for i in range(2,int(sqrt(n))+1):
+        if n%i == 0:
+            return False
+    return True
